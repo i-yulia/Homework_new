9965ea31d8d245c647f1056bb8fc4c778a7392c5
yulia <kimkaaa@mail.ru>
2022-09-01 01:29:19 +0300

    Task25,27,29

---
+++ Seminar/Seminar4/Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5-> 243(3⁵)
+
+int NumPower(int number1, int number2)
+{
+    int power = 1;
+    for (int i = 1; i <= number2; i++)
+    {
+        power = power * number1;
+    }
+    return power;
+}
+Console.WriteLine("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int Power = NumPower(numberA, numberB);
+Console.WriteLine($"{(Power)}");
---
+++ Seminar/Seminar4/Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+
+int SumNum(int num)
+{
+    int sum = 0;
+    for (int i = 0; 0 < num; i++)
+    {
+        sum = sum + num % 10;
+
+        num = num / 10;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int Sum = SumNum(number);
+Console.WriteLine($"{SumNum(number)}");
